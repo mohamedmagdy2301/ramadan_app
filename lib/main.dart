2de96ea0a4795b9fe216f82054c101835f700a22
mohamedmagdy2301 <mohamed.magdy@pure-soft.com>
2025-02-26 20:13:23 +0200

    Implement adaptive theme support and update main application structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,83 @@
+// Copyright © 2020 Birju Vachhani. All rights reserved.
+// Use of this source code is governed by an Apache license that can be
+// found in the LICENSE file.
+
+import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final savedThemeMode = await AdaptiveTheme.getThemeMode();
+  runApp(MyApp(savedThemeMode: savedThemeMode));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  final AdaptiveThemeMode? savedThemeMode;
+
+  const MyApp({super.key, this.savedThemeMode});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return AdaptiveTheme(
+      light: ThemeData.light(useMaterial3: true),
+      dark: ThemeData.dark(useMaterial3: true),
+      debugShowFloatingThemeButton: true,
+      initial: widget.savedThemeMode ?? AdaptiveThemeMode.system,
+      builder:
+          (theme, darkTheme) => MaterialApp(
+            theme: theme,
+            darkTheme: darkTheme,
+            home: MyHomePage(),
+          ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SizedBox.expand(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const Text('Light'),
+                  const SizedBox(width: 10),
+                  Switch.adaptive(
+                    value: AdaptiveTheme.of(context).mode.isDark,
+                    onChanged: (value) {
+                      if (value) {
+                        AdaptiveTheme.of(context).setDark();
+                      } else {
+                        AdaptiveTheme.of(context).setLight();
+                      }
+                    },
+                  ),
+                  const SizedBox(width: 10),
+                  const Text('Dark'),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }

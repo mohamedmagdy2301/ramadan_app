ca2e31324ec2950a63ce9ab5525f44cc4c004196
mohamedmagdy2301 <mohamed.magdy@pure-soft.com>
2025-03-08 02:41:28 +0200

    Remove PrayerTimeNotificationCubit and PrayerTimeNotificationState to streamline notification management

--- lib/features/home/presentation/view_model/notification_manager/prayer_time_notification_state.dart
+++
@@ -1,8 +0,0 @@
-// part of 'prayer_time_notification_cubit.dart';
-
-// @immutable
-// sealed class PrayerTimeNotificationState {}
-
-// final class PrayerTimeNotification extends PrayerTimeNotificationState {}
-
-// final class NoPrayerTimeNotification extends PrayerTimeNotificationState {}
--- lib/features/home/presentation/view_model/prayer_times_cubit/prayper_times_cubit.dart
+++ lib/features/home/presentation/view_model/prayer_times_cubit/prayper_times_cubit.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -14,55 +15,76 @@ part 'prayper_times_state.dart';
 class PrayerTimesCubit extends Cubit<PrayerTimesState> {
   PrayerTimesCubit() : super(PrayerTimesInitial()) {
     _scheduleDailyFetch();
+    startListeningToConnectivity();
   }
 
-  PrayerTimesRepository prayerTimesRepository = PrayerTimesRepository();
+  final PrayerTimesRepository prayerTimesRepository = PrayerTimesRepository();
   List<PrayerTimesEntity> prayerTimes = [];
-  Timer? _timer; // Timer variable
+  Timer? _timer;
+  StreamSubscription<List<ConnectivityResult>>? connectivitySubscription;
 
   Future<void> fetchPrayerTimes() async {
     emit(PrayerTimesLoading());
+
     try {
-      // Check internet connectivity
-      final isConnected = await _checkInternetConnection();
-      if (!isConnected) {
-        emit(PrayerTimesError("لا يوجد اتصال بالانترنت"));
+      if (!await isInternetAvailable()) {
+        emit(PrayerTimesError("لا يوجد اتصال بالإنترنت"));
+        return;
       }
 
-      // Get user location and fetch prayer times
       final position = await _determinePosition();
-      final locationName = await _getLocationName(position);
+      if (position == null) {
+        emit(PrayerTimesError("تعذر الحصول على الموقع"));
+        return;
+      }
 
+      final locationName = await _getLocationName(position);
       prayerTimes = await prayerTimesRepository.fetchPrayerTimes(
         location: position,
       );
+
       emit(PrayerTimesLoaded(prayerTimes, locationName));
     } catch (e) {
-      emit(PrayerTimesError(e.toString()));
+      emit(PrayerTimesError("حدث خطأ أثناء تحميل أوقات الصلاة"));
     }
   }
 
-  Future<bool> _checkInternetConnection() async {
-    final connectivityResult = await Connectivity().checkConnectivity();
-    return connectivityResult != ConnectivityResult.none;
+  Future<bool> isInternetAvailable() async {
+    try {
+      final result = await InternetAddress.lookup('google.com');
+      return result.isNotEmpty && result.first.rawAddress.isNotEmpty;
+    } catch (_) {
+      return false;
+    }
+  }
+
+  void startListeningToConnectivity() {
+    connectivitySubscription = Connectivity().onConnectivityChanged.listen((
+      List<ConnectivityResult> result,
+    ) {
+      print("Connectivity changed: $result");
+    });
   }
 
   void _scheduleDailyFetch() {
-    // Calculate the duration until the next midnight
     final now = DateTime.now();
     final nextMidnight = DateTime(now.year, now.month, now.day + 1);
     final durationUntilMidnight = nextMidnight.difference(now);
 
-    // Schedule the timer
-    _timer = Timer(durationUntilMidnight, () {
-      fetchPrayerTimes(); // Fetch prayer times at midnight
-      _scheduleDailyFetch(); // Reschedule for the next day
+    _timer = Timer(durationUntilMidnight, () async {
+      try {
+        await fetchPrayerTimes();
+      } catch (e) {
+        emit(PrayerTimesError(e.toString()));
+      }
+      _scheduleDailyFetch();
     });
   }
 
   @override
   Future<void> close() {
-    _timer?.cancel(); // Cancel the timer when closing
+    _timer?.cancel();
+    connectivitySubscription?.cancel();
     return super.close();
   }
 
@@ -82,24 +104,29 @@ class PrayerTimesCubit extends Cubit<PrayerTimesState> {
     }
   }
 
-  Future<Position> _determinePosition() async {
+  Future<Position?> _determinePosition() async {
     bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
     if (!serviceEnabled) {
-      return Future.error('خدمة تحديد الموقع غير مفعلة');
+      emit(PrayerTimesError('خدمة تحديد الموقع غير مفعلة'));
+      return null;
     }
 
     LocationPermission permission = await Geolocator.checkPermission();
     if (permission == LocationPermission.denied) {
       permission = await Geolocator.requestPermission();
       if (permission == LocationPermission.denied) {
-        return Future.error('الوصول للموقع مرفوض');
+        emit(PrayerTimesError('الوصول للموقع مرفوض'));
+        return null;
       }
     }
 
     if (permission == LocationPermission.deniedForever) {
-      return Future.error(
-        'الوصول للموقع مرفوض بشكل دائم، قم بتفعيله من إعدادات الجهاز',
+      emit(
+        PrayerTimesError(
+          'الوصول للموقع مرفوض بشكل دائم، قم بتفعيله من إعدادات الجهاز',
+        ),
       );
+      return null;
     }
 
     return await Geolocator.getCurrentPosition();

bb368a2736ab87258d6e3bbe090437ee01186609
mohamedmagdy2301 <mohamed.magdy@pure-soft.com>
2025-03-10 02:04:19 +0200

    update azkar screen

--- lib/features/azkar/presentation/view/widgets/azkar_details_liseview_item_card.dart
+++ lib/features/azkar/presentation/view/widgets/azkar_details_liseview_item_card.dart
@@ -6,7 +6,7 @@ import 'package:ramadan_app/core/extensions/int_extensions.dart' as ext;
 import 'package:ramadan_app/core/extensions/widget_extensions.dart';
 import 'package:ramadan_app/core/utils/functions/convert_num_to_ar.dart';
 
-class AzkarDetailsLiseviewItemCard extends StatelessWidget {
+class AzkarDetailsLiseviewItemCard extends StatefulWidget {
   final int index;
   final List<Map<String, String>>? dataList;
   final int counter;
@@ -21,91 +21,141 @@ class AzkarDetailsLiseviewItemCard extends StatelessWidget {
   });
 
   @override
+  State<AzkarDetailsLiseviewItemCard> createState() =>
+      _AzkarDetailsLiseviewItemCardState();
+}
+
+class _AzkarDetailsLiseviewItemCardState
+    extends State<AzkarDetailsLiseviewItemCard> {
+  bool isAnimating = false; // Animation state
+
+  void animateCircle() {
+    setState(() {
+      isAnimating = true;
+    });
+
+    Future.delayed(Duration(milliseconds: 150), () {
+      setState(() {
+        isAnimating = false;
+      });
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 10.h),
       child: GestureDetector(
-        onTap: onCounterChanged,
-        child: Card(
-          elevation: 4,
-          color:
-              context.isDark
-                  ? Colors.grey.shade900
-                  : const Color.fromARGB(255, 207, 207, 207),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10.r),
-          ),
-          child: Column(
-            children: [
-              Container(
-                width: double.infinity,
-                padding: EdgeInsets.symmetric(horizontal: 10.w, vertical: 10.h),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(10.r),
-                    topRight: Radius.circular(10.r),
+        onTap: () {
+          widget.onCounterChanged();
+          widget.counter ==
+                  (int.parse(widget.dataList?[widget.index]["count"] ?? "0"))
+              ? () {}
+              : animateCircle();
+        },
+        child: AnimatedContainer(
+          duration: Duration(milliseconds: 150),
+          child: Card(
+            elevation: 4,
+            color:
+                context.isDark
+                    ? Colors.grey.shade900
+                    : const Color.fromARGB(255, 207, 207, 207),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10.r),
+              side: BorderSide(
+                color: context.primaryColor,
+                width:
+                    isAnimating
+                        ? .6.w
+                        : widget.counter ==
+                            (int.parse(
+                              widget.dataList?[widget.index]["count"] ?? "0",
+                            ))
+                        ? .6.w
+                        : 0.1.w,
+              ),
+            ),
+            child: Column(
+              children: [
+                Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 10.w,
+                    vertical: 10.h,
                   ),
-                  border: Border.symmetric(
-                    vertical: BorderSide(
-                      color: context.primaryColor,
-                      width: .98.w,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(10.r),
+                      topRight: Radius.circular(10.r),
                     ),
-                    horizontal: BorderSide(
-                      color: context.primaryColor,
-                      width: .2.w,
-                    ),
-                  ),
-                  color:
-                      context.isDark
-                          ? const Color.fromARGB(255, 25, 25, 25)
-                          : const Color.fromARGB(255, 225, 225, 225),
-                ),
-                child: Row(
-                  children: [
-                    Text(
-                      "الذكر  ${convertNumberToArabic((index + 1).toString())}",
-                      style: StyleText.bold18().copyWith(
-                        color:
-                            counter ==
-                                    (int.parse(
-                                      dataList?[index]["count"] ?? "0",
-                                    ))
-                                ? context.primaryColor
-                                : context.onPrimaryColor,
+                    border: Border.symmetric(
+                      vertical: BorderSide(
+                        color: context.primaryColor,
+                        width: .7.w,
+                      ),
+                      horizontal: BorderSide(
+                        color: context.primaryColor,
+                        width: .2.w,
                       ),
                     ),
-                    Spacer(),
-                    counter == (int.parse(dataList?[index]["count"] ?? "0"))
-                        ? Icon(
-                          Icons.check_circle_outline,
-                          color: context.primaryColor,
-                        )
-                        : Text(
-                          "${convertNumberToArabic(removeZeroFromStart(counter.toString()))}  /  ${convertNumberToArabic((dataList?[index]["count"]).toString())}",
-                          style: StyleText.extraBold20().copyWith(
-                            color: context.onPrimaryColor,
-                          ),
-                        ),
-                    5.wSpace,
-                  ],
-                ),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  10.hSpace,
-                  Text(
-                    dataList?[index]['content'] ?? "",
-                    style: StyleText.regular26().copyWith(
-                      height: 2.h,
-                      wordSpacing: 2.w,
-                      fontFamily: "Amiri",
-                      color: context.onPrimaryColor,
-                    ),
+                    color:
+                        context.isDark
+                            ? const Color.fromARGB(255, 25, 25, 25)
+                            : const Color.fromARGB(255, 225, 225, 225),
                   ),
-                ],
-              ).paddingSymmetric(horizontal: 16, vertical: 15),
-            ],
+                  child: Row(
+                    children: [
+                      Text(
+                        "الذكر  ${convertNumberToArabic((widget.index + 1).toString())}",
+                        style: StyleText.bold18().copyWith(
+                          color:
+                              widget.counter ==
+                                      (int.parse(
+                                        widget.dataList?[widget
+                                                .index]["count"] ??
+                                            "0",
+                                      ))
+                                  ? context.primaryColor
+                                  : context.onPrimaryColor,
+                        ),
+                      ),
+                      Spacer(),
+                      widget.counter ==
+                              (int.parse(
+                                widget.dataList?[widget.index]["count"] ?? "0",
+                              ))
+                          ? Icon(
+                            Icons.check_circle_outline,
+                            color: context.primaryColor,
+                          )
+                          : Text(
+                            "${convertNumberToArabic(removeZeroFromStart(widget.counter.toString()))}  /  ${convertNumberToArabic((widget.dataList?[widget.index]["count"]).toString())}",
+                            style: StyleText.extraBold20().copyWith(
+                              color: context.onPrimaryColor,
+                            ),
+                          ),
+                      5.wSpace,
+                    ],
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    10.hSpace,
+                    Text(
+                      widget.dataList?[widget.index]['content'] ?? "",
+                      style: StyleText.regular26().copyWith(
+                        height: 2.h,
+                        wordSpacing: 2.w,
+                        fontFamily: "Amiri",
+                        color: context.onPrimaryColor,
+                      ),
+                    ),
+                  ],
+                ).paddingSymmetric(horizontal: 16, vertical: 15),
+              ],
+            ),
           ),
         ),
       ),

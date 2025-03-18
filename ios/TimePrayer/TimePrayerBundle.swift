c465954cad0eb72c649ce835d2a7dc61f3d1c91b
mohamedmagdy2301 <mohammedmego15@gmail.com>
2025-03-18 22:38:46 +0200

    Add iOS project files for TimePrayer widget

---
+++ ios/TimePrayer/TimePrayerBundle.swift
@@ -0,0 +1,16 @@
+//
+//  TimePrayerBundle.swift
+//  TimePrayer
+//
+//  Created by MohamedMego on 18/03/2025.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct TimePrayerBundle: WidgetBundle {
+    var body: some Widget {
+        TimePrayer()
+    }
+}

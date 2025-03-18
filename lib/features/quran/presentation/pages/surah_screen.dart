1d6f85fcd675fb753f21b0f617dcad55d8718d25
mohamedmagdy2301 <mohamed.magdy@pure-soft.com>
2025-03-18 13:23:34 +0200

    Refactor SurahScreen to improve reciter selection and enhance Ayah long press handling

--- lib/features/quran/presentation/pages/surah_screen.dart
+++ lib/features/quran/presentation/pages/surah_screen.dart
@@ -17,8 +17,9 @@ class SurahScreen extends StatefulWidget {
 }
 
 class _SurahScreenState extends State<SurahScreen> {
-  String selectedValue = 'mahermuaiqly';
-  final List<Map<String, String>> _dropdownItems = [
+  String selectedReciter = 'mahermuaiqly';
+
+  final List<Map<String, String>> reciters = [
     {"value": 'mahermuaiqly', "name": "ماهر المعيقلي"},
     {"value": 'abdulsamad', "name": "عبدالباسط عبدالصمد"},
     {"value": 'abdurrahmaansudais', "name": "عبدالرحمن السديس"},
@@ -29,16 +30,15 @@ class _SurahScreenState extends State<SurahScreen> {
     {"value": 'muhammadjibreel', "name": "محمد جبريل"},
   ];
 
-  bool? isShowToolBar = true;
-
   @override
   Widget build(BuildContext context) {
     final quranCtrl = Get.find<QuranCtrl>();
 
     return PopScope(
       onPopInvokedWithResult: (didPop, result) async {
-        if (quranCtrl.state.overlayEntry != null) {
+        if (didPop && quranCtrl.state.overlayEntry != null) {
           quranCtrl.state.overlayEntry?.remove();
+          quranCtrl.state.overlayEntry = null;
           navigatePop(context);
         }
       },
@@ -54,50 +54,59 @@ class _SurahScreenState extends State<SurahScreen> {
           textColor: context.onPrimaryColor,
           languageCode: "ar",
           surahNameStyle: SurahNameStyle(surahNameColor: context.primaryColor),
-          onDefaultAyahLongPress: (allBookmarks, details, ayah) {
-            final bookmarkId =
-                allBookmarks
-                    .firstWhereOrNull(
-                      (bookmark) => bookmark.ayahId == ayah.ayahUQNumber,
-                    )
-                    ?.id;
-            if (bookmarkId != null) {
-              BookmarksCtrl.instance.removeBookmark(bookmarkId);
-            } else {
-              quranCtrl.toggleAyahSelection(ayah.ayahUQNumber);
-              quranCtrl.state.overlayEntry?.remove();
-              quranCtrl.state.overlayEntry = null;
-              final overlay = Overlay.of(context);
-              final newOverlayEntry = OverlayEntry(
-                builder:
-                    (context) => _popWidgetToolsPar(details, ayah, context),
-              );
-              quranCtrl.state.overlayEntry = newOverlayEntry;
-              overlay.insert(newOverlayEntry);
-            }
-          },
+          onDefaultAyahLongPress: _handleAyahLongPress,
         ),
       ),
     );
   }
 
-  Positioned _popWidgetToolsPar(
+  void _handleAyahLongPress(
+    List<BookmarkModel> bookmarks,
+    LongPressStartDetails details,
+    AyahModel ayah,
+  ) {
+    final quranCtrl = Get.find<QuranCtrl>();
+    final bookmarkId =
+        bookmarks
+            .firstWhereOrNull(
+              (bookmark) => bookmark.ayahId == ayah.ayahUQNumber,
+            )
+            ?.id;
+
+    if (bookmarkId != null) {
+      BookmarksCtrl.instance.removeBookmark(bookmarkId);
+    } else {
+      quranCtrl.toggleAyahSelection(ayah.ayahUQNumber);
+      if (quranCtrl.state.overlayEntry != null) {
+        quranCtrl.state.overlayEntry?.remove();
+        quranCtrl.state.overlayEntry = null;
+      }
+
+      final overlay = Overlay.of(context);
+      final newOverlayEntry = OverlayEntry(
+        builder: (context) => _buildAyahOptionsOverlay(details, ayah, context),
+      );
+      quranCtrl.state.overlayEntry = newOverlayEntry;
+      overlay.insert(newOverlayEntry);
+    }
+  }
+
+  Positioned _buildAyahOptionsOverlay(
     LongPressStartDetails details,
     AyahModel ayah,
     BuildContext context,
   ) {
     return Positioned(
       top: 0,
-      left: -10.w,
-      right: -10.w,
+      left: 0.w,
+      right: 0.w,
       child: Container(
         color: context.backgroundColor,
         child: Container(
           padding: EdgeInsets.only(top: MediaQuery.paddingOf(context).top.h),
           alignment: Alignment.center,
-          color: context.primaryColor.withAlpha(40),
+          color: context.primaryColor.withAlpha(context.isDark ? 40 : 200),
           child: Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
               ...[0xAAFFD354, 0xAAF36077, 0xAA00CD00].map(
@@ -107,29 +116,28 @@ class _SurahScreenState extends State<SurahScreen> {
                 ).paddingSymmetric(horizontal: 6.w),
               ),
               context.verticalDivider(height: 30.h, color: Colors.white54),
-              // ClipboardAyahWidget(ayah: ayah),
-              // context.verticalDivider(
-              //   height: 30.h,
-              //   color: context.primaryColor,
-              // ),
-              AudioAyahWidget(ayah: ayah, selectedValue: selectedValue),
+              AudioAyahWidget(ayah: ayah, selectedValue: selectedReciter),
               context.verticalDivider(height: 30.h, color: Colors.white54),
-              SizedBox(
-                width: 130.w,
-                child: AppDropDownButton(
-                  hintText: ' ',
-                  value: selectedValue,
-                  onChanged: (value) {
-                    setState(() {
-                      selectedValue = value!;
-                    });
-                  },
-                  items: _dropdownItems,
-                ),
-              ),
+              _buildReciterDropdown(),
             ],
           ).paddingSymmetric(horizontal: 20),
         ),
+      ),
+    );
+  }
+
+  Widget _buildReciterDropdown() {
+    return SizedBox(
+      width: 130.w,
+      child: AppDropDownButton(
+        hintText: ' ',
+        value: selectedReciter,
+        onChanged: (value) {
+          if (value != null) {
+            setState(() => selectedReciter = value);
+          }
+        },
+        items: reciters,
       ),
     );
   }
